6abd5492d7498c5ed2847a623d72f9e5fdc67dc4
Alex Gaudio <adgaudio@gmail.com>
2015-04-21 23:40:39 -0400

    more ..

--- lockingqueue.py
+++ lockingqueue.py
@@ -7,6 +7,7 @@
 # f = findable set
 # expire_time = seconds_since_epoch + 5 seconds + whatever redlock figured out
 # client_id = owner of the lock, if we can obtain it.
+# returns 1 if got an item, and returns an error otherwise
 get_script = """
 local h_k = redis.call("ZRANGE", KEYS[1], 0, 0)[1]
 if nil == h_k then return {err="queue empty"} end
@@ -23,38 +24,40 @@ return h_k
 # f = findable set
 # h_k = time-ordered hash of key in form   priority:insert_time_since_epoch:key
 # expire_time = seconds_since_epoch + 5 seconds + whatever redlock figured out
-# random_seed = a random integer
+# random_seed = a random integer that changes every time script is called
 # client_id = owner of the lock, if we can obtain it.
-# TODO: test this
+# returns 1 if got lock. Returns an error otherwise
 lock_script = """
-if 0 == redis.call("SETNX", KEYS[1], ARGV[3]) then
-    -- did not get lock
-    if redis.call("GET", KEYS[1]) == "completed" then
-        if 1 ~= redis.call("ZREM", KEYS[2], KEYS[1]) then return end
-        return {err="already completed"}
-    else
-        score = redis.call("ZSCORE", KEYS[2], KEYS[1])
-        math.randomseed(tonumber(ARGV[2]))
-        num = math.random(math.floor(score) + 1)
-        if num != 1 then
-            redis.call("ZINCRBY", KEYS[2], (num-1)/score, KEYS[1])
-        return {err="already locked"}
+if 0 == redis.call("SETNX", KEYS[1], ARGV[3]) then  -- did not get lock
+  if redis.call("GET", KEYS[1]) == "completed" then
+    redis.call("ZREM", KEYS[2], KEYS[1])
+    return {err="already completed"}
+  else
+    local score = redis.call("ZSCORE", KEYS[2], KEYS[1])
+    math.randomseed(tonumber(ARGV[2]))
+    local num = math.random(math.floor(score) + 1)
+    if num ~= 1 then
+      redis.call("ZINCRBY", KEYS[2], (num-1)/score, KEYS[1])
     end
+    return {err="already locked"}
+  end
 else
-    redis.call("EXPIREAT", KEYS[1], ARGV[1])
-    redis.call("ZINCRBY", KEYS[2], 1, KEYS[1])
-    return 1  -- "got lock"
+  redis.call("EXPIREAT", KEYS[1], ARGV[1])
+  redis.call("ZINCRBY", KEYS[2], 1, KEYS[1])
+  return 1
 end
 """
 # eval lock_script 2 h_k f expire_time random_seed client_id
+# x=`echo $x + 1|bc`; redis-cli --eval ./hello.lua h_k1 f , $(echo `date +%s` + 5 | bc) $x bashclient
 
 
 # f = findable set
 # h_k = time-ordered hash of key in form   priority:insert_time_since_epoch:key
+# return 1 if removed, 0 if key was already removed. return val doesn't matter
 release_script = """
-SET KEYS[1] "completed"
-PERSIST KEYS[1]  # or perhaps EXPIRE some very large amount of time...
-ZREM KEYS[2] KEYS[1]
+redis.call("SET", KEYS[1], "completed")
+redis.call("PERSIST", KEYS[1])  -- or EXPIRE far into the future...
+return(redis.call("ZREM", KEYS[2], KEYS[1]))
 """
 # eval lock_script 2 h_k f
 
@@ -70,10 +73,14 @@ ZADD KEYS[1] 0 KEYS[2]
 # h_k = time-ordered hash of key in form   priority:insert_time_since_epoch:key
 # expire_time = seconds_since_epoch + 5 seconds + whatever redlock figured out
 # client_id = owner of the lock, if we can obtain it.
+# return 1 if extended lock, 0 if failed to extend lock.
+# TODO: test this out
 extend_lock = """
-if GET KEYS[1] == ARGV[2]
-    EXPIREAT KEYS[1] ARGV[1]
-else:
-    return "lost the lock!"
+if ARGV[2] == redis.call("GET", KEYS[1]) then
+    redis.call("EXPIREAT", KEYS[1], ARGV[1])
+    return 1
+else
+    return 0
+end
 """
 # eval extend_lock 1 h_k expire_time client_id
